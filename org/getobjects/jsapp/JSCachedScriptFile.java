351ecdf0fab43a061d1a5d3ae01727116b39e125
Helge Heß <helge.hess@opengroupware.org>
2008-07-06 15:14:11 +0000

    just added a few comments
    git-svn-id: https://getobjects.googlecode.com/svn/core/trunk@160 8aca4512-4549-0410-9768-61877113ad56

--- org/getobjects/jsapp/JSCachedScriptFile.java
+++ org/getobjects/jsapp/JSCachedScriptFile.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2007 Helge Hess <helge.hess@opengroupware.org>
+  Copyright (C) 2007-2008 Helge Hess <helge.hess@opengroupware.org>
   Copyright (C) 2007 Marcus Mueller <znek@mulle-kybernetik.com>
 
   This file is part of Go.
@@ -29,10 +29,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mozilla.javascript.Context;
 
+/**
+ * JSCachedScriptFile
+ * <p>
+ * The JSCachedObjectFile caches the contents of the given file as a Rhino
+ * 'Script' object, that is, as a compiled JavaScript source. This Script
+ * object can then be executed against arbitrary scopes (preferably global
+ * scopes which got a call to initStandardObjects).
+ * <p>
+ * Note: the subclass JSCachedScriptScope manages a fully prepared scope.
+ */
 public class JSCachedScriptFile extends JSCachedObjectFile {
   protected static final Log jslog = LogFactory.getLog("JSBridge");
   
-  public JSCachedScriptFile(File _dir, String _script) {
+  public JSCachedScriptFile(final File _dir, final String _script) {
     super(_dir, _script);
   }
   
@@ -44,9 +54,11 @@ public class JSCachedScriptFile extends JSCachedObjectFile {
    * @return a compiled Script object
    */
   @Override
-  public Object parseObject(String _path, Object _content) {
+  public Object parseObject(final String _path, final Object _content) {
     if (_content == null)
       return null;
+    
+    /* extract content */
     
     String src = null;
     
@@ -64,6 +76,8 @@ public class JSCachedScriptFile extends JSCachedObjectFile {
       jslog.warn("could not deal with JavaScript source: " + _path);
       return null;
     }
+    
+    /* compile script in current Rhino context */
     
     Context jscx = Context.getCurrentContext();
     if (jscx == null) { // TBD: log
