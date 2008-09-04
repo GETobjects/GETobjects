b8a96dc3707ba913d51e6bdc37780a4ccfe9d560
Marcus Müller <znek@mulle-kybernetik.com>
2008-09-04 16:24:30 +0000

    WEAutocompleteField accepts WOListWalker bindings now, as it implements all necessary repetition steps. The template of a WEAutocompleteField is supposed to be the content of a single completion item. All necessary wrapping (i.e. <ul><li>item1...</li>...) is done by WEAutocompleteField now. The previously optional fragment binding has been removed, as it is implicitly required now.

--- org/getobjects/weprototype/WEAutocompleteField.java
+++ org/getobjects/weprototype/WEAutocompleteField.java
@@ -23,12 +23,17 @@ package org.getobjects.weprototype;
 
 import java.util.Map;
 
+import org.getobjects.appserver.associations.WOValueAssociation;
 import org.getobjects.appserver.core.WOAssociation;
 import org.getobjects.appserver.core.WOContext;
+import org.getobjects.appserver.core.WODynamicElement;
 import org.getobjects.appserver.core.WOElement;
+import org.getobjects.appserver.core.WOElementWalker;
 import org.getobjects.appserver.core.WORequest;
 import org.getobjects.appserver.core.WOResponse;
+import org.getobjects.appserver.elements.WOGenericContainer;
 import org.getobjects.appserver.elements.WOJavaScriptWriter;
+import org.getobjects.appserver.elements.WORepetition;
 import org.getobjects.appserver.elements.links.WOLinkGenerator;
 
 /**
@@ -46,6 +51,21 @@ import org.getobjects.appserver.elements.links.WOLinkGenerator;
  *     indicator        = "progressIndicator";
  *   }</pre>
  * <p>
+ * Bindings:<pre>
+ *   name               [in] - string  (referring to the id of the WOTextField)
+ *   elementName        [in] - string  (default div)
+ *   class              [in] - string  (class to use for elementName)
+ *
+ *   tokens             [in] - string  (JS function or code)
+ *   updateElement      [in] - string  (JS function or code)
+ *   afterUpdateElement [in] - string  (JS function or code)
+ *   frequency          [in] - string  (default 0.4 s)
+ *   indicator          [in] - string  (element id)
+ *   select             [in] - string  (element id)
+ *   minChars           [in] - string</pre>
+ *
+ * TODO: document
+ * <p>
  * Bindings (WOLinkGenerator):<pre>
  *   href               [in] - string
  *   action             [in] - action
@@ -55,26 +75,25 @@ import org.getobjects.appserver.elements.links.WOLinkGenerator;
  *   fragmentIdentifier [in] - string
  *   queryDictionary    [in] - Map&lt;String,String&gt;
  *   - all bindings starting with a ? are stored as query parameters.</pre>
- * Bindings:<pre>
- *   name               [in] - string  (referring to the id of the WOTextField)
- *   elementName        [in] - string  (default div)
- *   class              [in] - string  (class to use for elementName)
- *   fragment           [in] - string  (if set, acts as a fragment)
- *
- *   tokens             [in] - string  (JS function or code)
- *   updateElement      [in] - string  (JS function or code)
- *   afterUpdateElement [in] - string  (JS function or code)
- *   frequency          [in] - string  (default 0.4 s)
- *   indicator          [in] - string  (element id)
- *   select             [in] - string  (element id)
- *   minChars           [in] - string
- *
- * TODO: document
+ * <p>
+ * WOListWalker Bindings:<pre>
+ *   list       [in]  - java.util.List | Collection | Java array | DOM Node
+ *   count      [in]  - int
+ *   item       [out] - object (current row)
+ *   index      [out] - int
+ *   startIndex [in]  - int
+ *   identifier [in]  - string (TODO: currently unescaped)
+ *   sublist    [in]  - java.util.List | Collection | Java array | DOM Node
+ *   isEven     [out] - boolean
+ *   isFirst    [out] - boolean
+ *   isLast     [out] - boolean
+ *   filter     [in]  - EOQualifier/String
+ *   sort       [in]  - EOSortOrdering/EOSortOrdering[]/Comparator/String/bool
+</pre>
  */
 public class WEAutocompleteField extends WEPrototypeElement {
 
   protected WOElement      template;
-  protected WOAssociation  fragment;
 
   protected WOAssociation   name;
   protected WOAssociation   elementName;
@@ -98,7 +117,6 @@ public class WEAutocompleteField extends WEPrototypeElement {
 
     this.name        = grabAssociation(_assocs, "name");
     this.elementName = grabAssociation(_assocs, "elementName");
-    this.fragment    = grabAssociation(_assocs, "fragment");
     this.clazz       = grabAssociation(_assocs, "class");
 
     this.tokens             = grabAssociation(_assocs, "tokens");
@@ -109,11 +127,25 @@ public class WEAutocompleteField extends WEPrototypeElement {
     this.updateElement      = grabAssociation(_assocs, "updateElement");
     this.afterUpdateElement = grabAssociation(_assocs, "afterUpdateElement");
 
-    if (this.fragment != null) {
-      _assocs.put("?wofid", this.fragment);
-      this.template = _template;
-    }
+
+    String fragmentID = this.name.stringValueInComponent(null) +
+                        "_autocompletion";
+    _assocs.put("?wofid", new WOValueAssociation(fragmentID));
+
     this.link = WOLinkGenerator.linkGeneratorForAssociations(_assocs);
+
+    /* construct the template */
+
+    // template needs to be wrapped in a <ul>...</ul>, and each element of
+    // the repetition needs to be wrapped in <li>...</li>
+
+    _assocs.put("elementName", new WOValueAssociation("li"));
+    WODynamicElement element =
+      new WORepetition(fragmentID + "_rep", _assocs, _template);
+
+    _assocs.put("elementName", new WOValueAssociation("ul"));
+    element = new WOGenericContainer("ul", _assocs, element);
+    this.template = element;
   }
 
   /* support */
@@ -137,22 +169,16 @@ public class WEAutocompleteField extends WEPrototypeElement {
   }
 
   protected String autocompleteElementNameInContext(final WOContext _ctx) {
-    return this.inputElementNameInContext(_ctx) + "_autocomplete";
+    return this.inputElementNameInContext(_ctx) + "_autocompletion";
   }
 
   protected boolean isFragmentActiveInContext(WOContext _ctx) {
-    if (this.fragment == null) return false;
-
     String rqFragID = _ctx.fragmentID();
     if (rqFragID == null) {
       return false;
     }
 
-    String fragName = strForAssoc(this.fragment, _ctx.cursor());
-    if (fragName == null) { /* we have no fragid in the current state */
-      return false; /* we only render on match */
-    }
-
+    String fragName = autocompleteElementNameInContext(_ctx);
     if (!rqFragID.equals(fragName)) {
       return false;
     }
@@ -234,10 +260,9 @@ public class WEAutocompleteField extends WEPrototypeElement {
       return;
 
     if (isFragmentActive) {
-      if (this.template != null)
-        _ctx.enableRendering();
-        this.template.appendToResponse(_r, _ctx);
-        _ctx.disableRendering();
+      _ctx.enableRendering();
+      this.template.appendToResponse(_r, _ctx);
+      _ctx.disableRendering();
     }
     else {
       String tagName = strForAssoc(this.elementName, _ctx.cursor());
@@ -261,5 +286,13 @@ public class WEAutocompleteField extends WEPrototypeElement {
       this.appendJavaScript(js, _ctx);
       _r.appendContentScript(js);
     }
+  }
+
+  /* generic walker */
+
+  @Override
+  public void walkTemplate(WOElementWalker _walker, WOContext _ctx) {
+    if (this.template != null)
+      _walker.processTemplate(this, this.template, _ctx);
   }
 }
