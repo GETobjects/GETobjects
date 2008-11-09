5b7852057743deb497634468ed059b776f83f229
Helge Heß <helge.hess@opengroupware.org>
2008-11-09 19:39:55 +0000

    added explicit name/value bindings (worked before)
    git-svn-id: https://getobjects.googlecode.com/svn/core/trunk@225 8aca4512-4549-0410-9768-61877113ad56

--- org/getobjects/appserver/elements/WOParam.java
+++ org/getobjects/appserver/elements/WOParam.java
@@ -1,3 +1,23 @@
+/*
+  Copyright (C) 2007-2008 Helge Hess <helge.hess@opengroupware.org>
+
+  This file is part of Go.
+
+  Go is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  Go is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Go; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
 package org.getobjects.appserver.elements;
 
 import java.util.Map;
@@ -11,11 +31,17 @@ import org.getobjects.appserver.core.WOResponse;
  * WOParam
  * <p>
  * Parameter value for applets.
+ * 
  * <p>
- * TODO: document
- * TODO: WO also allows for an 'action' binding. Not sure whethers thats useful.
+ * Bindings (WOLinkGenerator for image resource):
+ * <pre>
+ *   name  [in] - string
+ *   value [in] - string</pre>
  */
 public class WOParam extends WOHTMLDynamicElement {
+  // TODO: document
+  // TODO: WO also allows for an 'action' binding. Not sure whethers thats 
+  //       useful.
   
   protected WOAssociation name;
   protected WOAssociation value;
@@ -24,13 +50,16 @@ public class WOParam extends WOHTMLDynamicElement {
     (String _name, Map<String, WOAssociation> _assocs, WOElement _template)
   {
     super(_name, _assocs, _template);
+    
+    this.name  = grabAssociation(_assocs, "name");
+    this.value = grabAssociation(_assocs, "value");
   }
   
   /* generate responds */
 
   @Override
-  public void appendToResponse(WOResponse _r, WOContext _ctx) {
-    Object cursor = _ctx.cursor();
+  public void appendToResponse(final WOResponse _r, final WOContext _ctx) {
+    final Object cursor = _ctx.cursor();
     
     _r.appendBeginTag("param");
     
@@ -49,7 +78,7 @@ public class WOParam extends WOHTMLDynamicElement {
   /* description */
   
   @Override
-  public void appendAttributesToDescription(StringBuilder _d) {
+  public void appendAttributesToDescription(final StringBuilder _d) {
     super.appendAttributesToDescription(_d);
     
     this.appendAssocToDescription(_d, "name",  this.name);
