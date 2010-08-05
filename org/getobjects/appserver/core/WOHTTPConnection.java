ffe2c44d11ff3402b3d2b75feb73656ae6738b9e
Marcus Müller <znek@mulle-kybernetik.com>
2010-08-05 12:44:48 +0000

    Implemented HTTPS connections. Per default, only trusted SSL connections can be made (trusted as per keystore content). It's also possible to allow insecure SSL connections, thus using self-signed certificates without having to deal with the keystore.
    git-svn-id: https://getobjects.googlecode.com/svn/core/trunk@334 8aca4512-4549-0410-9768-61877113ad56

--- org/getobjects/appserver/core/WOHTTPConnection.java
+++ org/getobjects/appserver/core/WOHTTPConnection.java
@@ -25,9 +25,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -36,37 +48,51 @@ import org.getobjects.foundation.UData;
 import org.getobjects.foundation.UObject;
 import org.getobjects.foundation.UString;
 
-public class WOHTTPConnection extends NSObject {
+public class WOHTTPConnection extends NSObject implements HostnameVerifier {
 
   protected static final Log log = LogFactory.getLog("WOHTTPConnection");
 
   protected String  host;
   protected int     port;
-  protected int     receiveTimeout  = 30 * 1000; // milliseconds
-  protected int     sendTimeout     = 10 * 1000; // milliseconds
-  protected int     readTimeout     = 0;
-  protected boolean followRedirects = true;
-  protected boolean keepAlive       = true;
+  protected int     receiveTimeout   = 30 * 1000; // milliseconds
+  protected int     sendTimeout      = 10 * 1000; // milliseconds
+  protected int     readTimeout      = 0;
+  protected boolean followRedirects  = true;
+  protected boolean keepAlive        = true;
+  protected boolean isSSL            = false;
+  protected boolean allowInsecureSSL = false;
 
   protected HttpURLConnection urlConnection;
   protected WORequest request; // last used request, attached to response
 
 
+  /**
+   * Creates an 'http' connection to _host on _port.
+   */
   public WOHTTPConnection(String _host, int _port) {
     this.host = _host;
     this.port = _port;
   }
 
+  /**
+   * Creates an 'http' connection to _host on port 80.
+   */
   public WOHTTPConnection(String _host) {
     this(_host, 80);
   }
 
+  /**
+   * Creates either an 'http' connection or an 'https' connection
+   * to the host specified in _url.
+   */
   public WOHTTPConnection(URL _url) {
     this.host = _url.getHost();
     this.port = _url.getPort();
-    if (!_url.getProtocol().toLowerCase().equals("http")) {
+    String protocol = _url.getProtocol().toLowerCase();
+    if (protocol.equals("https"))
+      this.isSSL = true;
+    else if (!protocol.equals("http"))
       log.warn("given URL not for HTTP connection: " + _url);
-    }
   }
 
   // Accessors
@@ -114,13 +140,82 @@ public class WOHTTPConnection extends NSObject {
     return this.followRedirects;
   }
 
+  /**
+   * (GETobjects extension):
+   * If true, allow connections to SSL sites whose certs are not trusted by a
+   * certificate authority provided in the Java keystore.
+   * If false, only allow SSL connections to SSL sites whose certificates
+   * are trusted by the Java keystore.
+   *
+   * NOTE: the keystore which the JavaVM uses can be set upon launch
+   *       as the 'javax.net.ssl.trustStore' system property value.
+   */
+  public void setAllowInsecureHttpsConnection(boolean _tf) {
+    this.allowInsecureSSL = _tf;
+  }
+
+  /**
+   * (GETobjects extension):
+   * If true, no trust is implied when performing an SSL connection.
+   * The default is 'false' which means only trusted connections can be made.
+   */
+  public boolean allowInsecureHttpsConnection() {
+    return this.allowInsecureSSL;
+  }
+
+  // Setup
+
+  /**
+   * (GETobjects extension):
+   * Called, whenever a new urlConnection is setup.
+   *
+   * Subclasses may override or extend the steps taken.
+   */
+  protected void setupHttpURLConnection(HttpURLConnection _conn) {
+    // According to WO53 documentation, this is indeed the first sendTimeout
+    _conn.setConnectTimeout(this.sendTimeout);
+    _conn.setAllowUserInteraction(false);
+    _conn.setDoInput(true);
+  }
+
+  /**
+   * (GETobjects extension):
+   * Called, whenever a new urlConnection is setup and only if it's a
+   * HttpsURLConnection.
+   *
+   * This method is called in addition to
+   * setupHttpURLConnection() and should contain refinement necessary for
+   * SSL purposes, only.
+   *
+   * Subclasses may override or extend the steps taken.
+   */
+  protected void setupHttpsURLConnection(HttpsURLConnection _conn) {
+    _conn.setHostnameVerifier(this);
+
+    if (this.allowInsecureSSL) {
+      try {
+        _conn.setSSLSocketFactory(new GullibleSSLSocketFactory(null));
+      }
+      catch (GeneralSecurityException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  // SSL
+
+  public boolean verify(String _hostname, SSLSession _session) {
+    return true;
+  }
+
   // Request
 
   public boolean sendRequest(WORequest _rq) {
     if (_rq == null) return false;
 
     try {
-      URL     url        = new URL("http", this.host, this.port, _rq.uri());
+      URL     url        = new URL(this.isSSL ? "https" : "http",
+                                   this.host, this.port, _rq.uri());
       boolean needsSetup = true;
 
       if (this.urlConnection != null &&
@@ -130,11 +225,12 @@ public class WOHTTPConnection extends NSObject {
       }
 
       if (needsSetup) {
-          this.urlConnection = (HttpURLConnection)url.openConnection();
-          // According to WO53 docs, this is indeed the first sendTimeout
-          this.urlConnection.setConnectTimeout(this.sendTimeout);
-          this.urlConnection.setAllowUserInteraction(false);
-          this.urlConnection.setDoInput(true);
+        this.urlConnection = (HttpURLConnection)url.openConnection();
+
+        this.setupHttpURLConnection(this.urlConnection);
+        if (this.urlConnection instanceof HttpsURLConnection) {
+          this.setupHttpsURLConnection((HttpsURLConnection)this.urlConnection);
+        }
       }
 
       // set properties and headers
@@ -262,5 +358,83 @@ public class WOHTTPConnection extends NSObject {
       log.error("readResponse() failed: " + e);
     }
     return r;
+  }
+
+
+  // SSL Helpers
+
+  /**
+   * GullibleSSLSocketFactory provides SSL sockets which accept all server
+   * certificates.
+   *
+   * Apart from performing encryption, no 'trust' checking is done on any
+   * certificate involved. Connections using these sockets are vulnerable
+   * to man in the middle attacks!
+   */
+  protected class GullibleSSLSocketFactory extends SSLSocketFactory {
+
+    /**
+     * GullibleTrustManager doesn't implement any trust management at all.
+     */
+    protected class GullibleTrustManager implements X509TrustManager {
+
+      public X509Certificate[] getAcceptedIssuers() {
+          return new X509Certificate[0];
+      }
+
+      public void checkClientTrusted(X509Certificate[] _certs, String _authType)
+      {
+      }
+
+      public void checkServerTrusted(X509Certificate[] _certs, String _authType)
+      {
+      }
+    }
+
+    protected SSLSocketFactory factory;
+
+    public GullibleSSLSocketFactory(String _unused)
+    throws GeneralSecurityException
+    {
+        SSLContext ctx = SSLContext.getInstance("TLS");
+        ctx.init(null, new TrustManager[] { new GullibleTrustManager() }, null);
+        this.factory = ctx.getSocketFactory();
+    }
+
+    public Socket createSocket(InetAddress _host, int _port) throws IOException
+    {
+      return this.factory.createSocket(_host, _port);
+    }
+
+    public Socket createSocket(String _host, int _port) throws IOException {
+      return this.factory.createSocket(_host, _port);
+    }
+
+    public Socket createSocket(String _host, int _port, InetAddress localHost,
+        int localPort) throws IOException
+    {
+      return this.factory.createSocket(_host, _port, localHost, localPort);
+    }
+
+    public Socket createSocket(InetAddress _address, int _port,
+        InetAddress _localAddress, int _localPort) throws IOException
+    {
+      return this.factory.createSocket(_address, _port, _localAddress,
+          _localPort);
+    }
+
+    public Socket createSocket(Socket _socket, String _host, int _port,
+        boolean _autoClose) throws IOException
+    {
+      return this.factory.createSocket(_socket, _host, _port, _autoClose);
+    }
+
+    public String[] getDefaultCipherSuites() {
+      return this.factory.getDefaultCipherSuites();
+    }
+
+    public String[] getSupportedCipherSuites() {
+      return this.factory.getSupportedCipherSuites();
+    }
   }
 }
