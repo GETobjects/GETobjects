badfce458a1a13593b66572f4cf1c3a354e6962f
Marcus Müller <znek@mulle-kybernetik.com>
2014-02-26 12:35:27 +0000

    Fixed missing "method" binding processing.
    git-svn-id: https://getobjects.googlecode.com/svn/core/trunk@355 8aca4512-4549-0410-9768-61877113ad56

--- org/getobjects/weprototype/WELinkToRemoteScript.java
+++ org/getobjects/weprototype/WELinkToRemoteScript.java
@@ -53,15 +53,15 @@ import org.getobjects.foundation.UString;
  *     update           = "time_div";
  *     position         = "after";
  *   }</pre>
- * 
+ *
  * Template:
  * <pre>
  *   &lt;a href="#" &lt;#OnClick/&gt;&gt;Do it!&lt;/a&gt;</pre>
- *   
+ *
  * Renders:
  * <pre>
  *   TODO: document</pre>
- * 
+ *
  * Bindings (WOLinkGenerator):
  * <pre>
  *   href               [in] - string
@@ -81,7 +81,7 @@ import org.getobjects.foundation.UString;
  *   position           [in] - string      (before/top/bottom/after/..)
  *   method             [in] - string      (GET or POST)
  *   event              [in] - string      (event handler to use (def: onclick))
- *   
+ *
  *   confirm            [in] - string (JS function or code)
  *   condition          [in] - string (JS function or code)
  *   before             [in] - string (JS function or code)
@@ -109,7 +109,7 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
   public static final int PARAMETERMODE_FORM        = 1;
   public static final int PARAMETERMODE_OBSERVE     = 2;
   public static final int PARAMETERMODE_FORMELEMENT = 3;
-  
+
   protected WOAssociation   type;
   protected WOAssociation   update; /* DOM element 'id' to update */
   protected WOAssociation   position;
@@ -124,24 +124,24 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
 
   protected WOAssociation   accept;
   protected WOAssociation   headers;
-  
+
   protected WOAssociation   fragment;
-  
+
   protected WOLinkGenerator link;
-  
+
   /* callbacks */
   protected Map<String, WOAssociation> callbacks;
-  
+
   /* special WOFormToRemote support */
   protected int parameterMode;
-  
+
   /* special WOObserveField support */
   protected WOAssociation with;
   protected WOAssociation name;
-  
+
   public void init(String _name, Map<String, WOAssociation> _assocs) {
     this.parameterMode = PARAMETERMODE_LINK;
-    
+
     this.type      = grabAssociation(_assocs, "type");
     this.update    = grabAssociation(_assocs, "update");
     this.position  = grabAssociation(_assocs, "position");
@@ -153,20 +153,20 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
     this.before    = grabAssociation(_assocs, "before");
     this.after     = grabAssociation(_assocs, "after");
     this.submit    = grabAssociation(_assocs, "submit");
-    
+
     this.accept    = grabAssociation(_assocs, "accept");
     this.headers   = grabAssociation(_assocs, "headers");
-    
+
     /* callbacks */
-    
+
     this.callbacks = extractCallbacks(_assocs);
-    
-    
+
+
     /* special fragment support */
-    
+
     this.fragment = grabAssociation(_assocs, "fragment");
     if (this.update == null) this.update = this.fragment;
-    
+
     if (this.fragment != null && this.link == null) {
       if (!WOLinkGenerator.containsLinkInAssociations(_assocs)) {
         /* user specified no link, so we just call default */
@@ -175,38 +175,38 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
     }
 
     /* link and template */
-    
+
     // System.err.println("A: " + _assocs);
     if (this.link == null)
       this.link = WOLinkGenerator.linkGeneratorForAssociations(_assocs);
   }
-  
+
   public WELinkToRemoteScript
     (String _name, Map<String, WOAssociation> _assocs, WOElement _template)
   {
     super(_name, _assocs, _template);
     this.init(_name, _assocs);
   }
-  
+
   public WELinkToRemoteScript
     (String _name, Map<String, WOAssociation> _assocs, int _parameterMode,
      WOLinkGenerator _link)
   {
     super(_name, _assocs, null /* template */);
-    
+
     if (_link != null)
       this.link = _link; /* this makes the initializer skip the linkscan */
-    
+
     this.init(_name, _assocs);
-    
+
     this.parameterMode = _parameterMode;
-    
+
     if (this.parameterMode == PARAMETERMODE_OBSERVE) {
       this.name = grabAssociation(_assocs, "name");
       this.with = grabAssociation(_assocs, "with");
     }
   }
-  
+
   /* helper */
 
   protected static Map<String, WOAssociation> extractCallbacks
@@ -217,12 +217,12 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
 
     Map<String, WOAssociation> cbs = new HashMap<String, WOAssociation>(8);
     List<String> keysToRemove = new ArrayList<String>(8);
-    
+
     for (String name: _assocs.keySet()) {
       String cb = null;
       char c0 = name.charAt(0);
       int  nl = name.length();
-      
+
       switch (c0) {
         case 'c':
           if (name.equals("complete")) cb = "onComplete";
@@ -246,42 +246,42 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
         case 'u':
           if (name.equals("uninitialized")) cb = "onUninitialized";
           break;
-        
+
         case '1': case '2': case '3': case '4': case '5': /* HTTP status */
           if (nl == 3) /* eg 404 => on404 */
             cb = "on" + name;
           break;
       }
-      
+
       if (cb != null) {
         cbs.put(cb, _assocs.get(name));
         keysToRemove.add(name);
       }
     }
-    
+
     for (String k: keysToRemove)
       _assocs.remove(k);
-    
+
     return (cbs.size() == 0) ? null : cbs;
   }
-  
-  
+
+
   /* accessors (required by some wrapping elements */
-  
+
   public WOLinkGenerator getLink() {
     return this.link;
   }
-  
+
 
   /* handle requests */
-  
+
   public void takeValuesFromRequest(WORequest _rq, WOContext _ctx) {
     /* links can take form values !!!! (for query-parameters) */
-    
+
     if (this.link != null)
       this.link.takeValuesFromRequest(_rq, _ctx);
   }
-  
+
   public Object invokeAction(WORequest _rq, WOContext _ctx) {
     if (_ctx.elementID().equals(_ctx.senderID())) {
       if (this.link != null)
@@ -295,19 +295,19 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
   }
 
   /* generate response */
-  
+
   public static boolean isSimpleJavaScriptID(String _js) {
     if (_js == null)
       return false;
-    
+
     boolean isFirstChar = true;
     for (int i = 0, len = _js.length(); i < len; i++) {
       char c = _js.charAt(i);
-      
+
       if (isFirstChar) {
         if (!Character.isJavaIdentifierStart(c))
           return false;
-        
+
         isFirstChar = false;
       }
       else if (c == '.') {
@@ -320,11 +320,11 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
     }
     return true;
   }
-  
+
   protected void appendCallbacksToJS(WOJavaScriptWriter _js, Object cursor) {
     if (this.callbacks == null)
       return;
-    
+
     String s;
     for (String cn: this.callbacks.keySet()) {
       if ((s = strForAssoc(this.callbacks.get(cn), cursor)) == null)
@@ -340,27 +340,27 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
        *       properly defined.
        */
       _js.append("function(request,json){");
-      
+
       if (isSimpleJavaScriptID(s)) {
         _js.append(s);
         _js.append("(request,json);");
       }
       else
         _js.append(s);
-      
+
       _js.append("}");
     }
   }
-  
+
   @SuppressWarnings("unchecked")
   protected void appendHeadersToJS(WOJavaScriptWriter _js, Object cursor) {
     Map<String, Object> lRequestHeaders = null;
-    
+
     if (this.headers != null) {
-      lRequestHeaders = (Map<String, Object>) 
+      lRequestHeaders = (Map<String, Object>)
         this.headers.valueInComponent(cursor);
     }
-    
+
     if (this.accept != null) {
       Object v = this.accept.valueInComponent(cursor);
       if (v != null) {
@@ -369,7 +369,7 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
         else
           lRequestHeaders = new HashMap<String, Object>(lRequestHeaders);
       }
-      
+
       if (v instanceof String)
         lRequestHeaders.put("Accept", v);
       else if (v instanceof List) {
@@ -384,30 +384,30 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
         log.warn("unexpected value for Accept header: " + v);
       }
     }
-    
+
     if (lRequestHeaders != null && lRequestHeaders.size() > 0) {
       _js.append(", requestHeaders: [ ");
-      
+
       boolean isFirst = true;
       for (String key: lRequestHeaders.keySet()) {
         if (isFirst) isFirst = false;
         else _js.append(", ");
-        
+
         _js.appendString(key);
         _js.append(", ");
         _js.appendConstant(lRequestHeaders.get(key));
       }
       _js.append("]");
     }
-    
+
   }
-  
+
   public void appendJavaScript(WOJavaScriptWriter _js, WOContext _ctx) {
     Object cursor =_ctx.cursor();
-    
+
     String s;
     int braceCloseCount = 0;
-    
+
     /* confirm panel */
     if ((s = strForAssoc(this.confirm, cursor)) != null) {
       _js.append("if (");
@@ -423,42 +423,42 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
       _js.append(") { ");
       braceCloseCount++;
     }
-    
+
     /* before */
     if ((s = strForAssoc(this.before, cursor)) != null)
       _js.append(s);
-    
+
     /* now generate AJAX call */
-    
+
     this.beginAjaxCallWithUpdater(_js,
         this.update != null ? this.update.valueInComponent(cursor) : null);
-    
-    
+
+
     /* generate link parameter */
 
     s = (this.link != null) ? this.link.fullHrefInContext(_ctx) : null;
-    
+
     String fragId = this.fragment != null
       ? this.fragment.stringValueInComponent(cursor) : null;
-      
+
     if (s == null) s = fragId != null ? _ctx.request().uri() : "#";
-    
+
     if (fragId != null) {
       /* We do add the fragment id manually because the regular FORM action
        * should invoke the action w/o a fragment. That is, the link generator
        * will return a URL w/o a fragment-id.
        */
       fragId = WORequest.FragmentIDKey + "=" + fragId;
-      s += (s.indexOf('?') < 0) ? ("?" + fragId) : ("&" + fragId); 
+      s += (s.indexOf('?') < 0) ? ("?" + fragId) : ("&" + fragId);
     }
     _js.appendConstant(s);
     _js.append(", ");
-    
-    
+
+
     /* generate options dictionary */
 
     _js.beginMap();
-    
+
     /* type */
     if (this.type != null) {
       _js.append(this.type.stringValueInComponent(cursor));
@@ -466,26 +466,32 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
     }
     else
       _js.append("asynchronous:true");
-    
+
     /* eval */
     _js.append(", evalScripts:true"); // this evaluates scripts in the content
-    
+
+    /* method */
+    if (this.method != null) {
+      _js.append(", method:");
+      _js.appendString(this.method.stringValueInComponent(cursor));
+    }
+
     /* headers */
-    
+
     if (this.headers != null || this.accept != null)
       this.appendHeadersToJS(_js, cursor);
-    
+
     /* position */
     if ((s = strForAssoc(this.position, cursor)) != null) {
       if (s.length() > 0) {
         if (!Character.isUpperCase(s.charAt(0)))
-          s = "" + Character.toUpperCase(s.charAt(0)) + s.substring(1); 
-        
+          s = "" + Character.toUpperCase(s.charAt(0)) + s.substring(1);
+
         _js.append(", insertion:Insertion.");
         _js.append(s);
       }
     }
-    
+
     /* form parameters */
     if ((s = strForAssoc(this.submit, cursor)) != null) {
       _js.append(", parameters:Form.serialize(");
@@ -515,26 +521,26 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
         }
         else
           _js.append("value");
-      }      
+      }
     }
-    
+
     /* callbacks */
-    
+
     if (this.callbacks != null)
       this.appendCallbacksToJS(_js, cursor);
-    
+
     /* close AJAX call */
     _js.append("});");
-    
+
     /* after */
     if ((s = strForAssoc(this.after, cursor)) != null)
       _js.append(s);
-    
+
     /* finish JavaScript and add to anker */
-    
+
     for (int i = 0; i < braceCloseCount; i++)
       _js.append(" };");
-    
+
     _js.append("return false");
   }
 
@@ -542,13 +548,13 @@ public class WELinkToRemoteScript extends WEPrototypeElement {
   public void appendToResponse(WOResponse _r, WOContext _ctx) {
     if (_ctx.isRenderingDisabled())
       return;
-    
+
     WOJavaScriptWriter js = new WOJavaScriptWriter();
     this.appendJavaScript(js, _ctx);
-    
+
     if (this.event != null) {
       String onWhat = this.event.stringValueInComponent(_ctx.cursor());
-      
+
       if (onWhat != null)
         _r.appendAttribute(onWhat, js.script());
       else {
