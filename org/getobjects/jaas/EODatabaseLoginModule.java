d74d8d4c80087e2ea5be15423f86b669e0bee1a0
Helge Heß <me@helgehess.eu>
2014-05-30 12:59:13 +0000

    Cleanup licenses.
    git-svn-id: https://getobjects.googlecode.com/svn/core/trunk@442 8aca4512-4549-0410-9768-61877113ad56

--- org/getobjects/jaas/EODatabaseLoginModule.java
+++ org/getobjects/jaas/EODatabaseLoginModule.java
@@ -1,20 +1,20 @@
 /*
-  Copyright (C) 2008 Helge Hess <helge.hess@opengroupware.org>
+  Copyright (C) 2008-2014 Helge Hess
 
-  This file is part of OpenGroupware.org (OGo)
+  This file is part of Go.
 
-  OGo is free software; you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the
+  Go is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
   Free Software Foundation; either version 2, or (at your option) any
   later version.
 
-  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  Go is distributed in the hope that it will be useful, but WITHOUT ANY
   WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
   License for more details.
 
-  You should have received a copy of the GNU General Public
-  License along with OGo; see the file COPYING.  If not, write to the
+  You should have received a copy of the GNU Lesser General Public
+  License along with Go; see the file COPYING.  If not, write to the
   Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
   02111-1307, USA.
 */
--- org/getobjects/jaas/GoDefaultPrincipal.java
+++ org/getobjects/jaas/GoDefaultPrincipal.java
@@ -1,3 +1,23 @@
+/*
+  Copyright (C) 2014 Helge Hess <helge.hess@opengroupware.org>
+
+  This file is part of GETobjects (Go)
+
+  Go is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  Go is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
 package org.getobjects.jaas;
 
 import java.security.Principal;
