7d1a5d00ffde8330a1607d6734479db71e83e356
Marcus Müller <znek@mulle-kybernetik.com>
2016-04-13 00:50:12 +0200

    WOHTTPConnection had multiple issues while using java.net.HttpURLConnection as a transport due to a number of shortcomings, the most annoying being timeouts completely ignored by SUN's implementation, rendering connection issues unpredictable. This new implementation utilizes Jetty 9.2's HttpClient infrastructure which promises to overcome all the previous issues, while also making the implementation cleaner.

--- org/getobjects/appserver/core/WOHTTPConnection.java
+++ org/getobjects/appserver/core/WOHTTPConnection.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2010 Marcus Mueller
+  Copyright (C) 2010, 2016 Marcus Mueller
 
   This file is part of Go.
 
@@ -21,42 +21,32 @@
 
 package org.getobjects.appserver.core;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Authenticator;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
+import java.net.HttpCookie;
 import java.net.MalformedURLException;
-import java.net.PasswordAuthentication;
-import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.security.GeneralSecurityException;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.Authentication;
+import org.eclipse.jetty.client.api.AuthenticationStore;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.util.BasicAuthentication;
+import org.eclipse.jetty.client.util.BytesContentProvider;
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpFields;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.getobjects.foundation.NSObject;
-import org.getobjects.foundation.UData;
 import org.getobjects.foundation.UObject;
-import org.getobjects.foundation.UString;
 
-public class WOHTTPConnection extends NSObject implements HostnameVerifier {
+public class WOHTTPConnection extends NSObject {
 
   protected static final Log log = LogFactory.getLog("WOHTTPConnection");
-
-  protected static final Authenticator sharedURLAuthorityBasedAuthenticator =
-    new URLAuthorityBasedAuthenticator();
 
   protected URL     url;
   protected int     receiveTimeout   = 30 * 1000; // milliseconds
@@ -66,9 +56,9 @@ public class WOHTTPConnection extends NSObject implements HostnameVerifier {
   protected boolean keepAlive        = true;
   protected boolean isSSL            = false;
   protected boolean allowInsecureSSL = false;
-  protected Authenticator authenticator = sharedURLAuthorityBasedAuthenticator;
 
-  protected HttpURLConnection urlConnection;
+  protected HttpClient httpClient;
+  protected ContentResponse contentResponse;
   protected WORequest request; // last used request, attached to response
 
 
@@ -123,9 +113,8 @@ public class WOHTTPConnection extends NSObject implements HostnameVerifier {
 
   // NOTE: this cast is for conformance with the WebObjects specification -
   // I don't think it'll do any harm
-  @SuppressWarnings("cast")
   public long readTimeout() {
-    return (long)this.readTimeout;
+    return this.readTimeout;
   }
 
   public void setSendTimeout(int _sendTimeout) {
@@ -174,79 +163,19 @@ public class WOHTTPConnection extends NSObject implements HostnameVerifier {
   public boolean allowInsecureHttpsConnection() {
     return this.allowInsecureSSL;
   }
-
-  /**
-   * (GETobjects extension):
-   * Sets the authenticator to use for this WOHTTPConnection, if authentication
-   * is required.
-   */
-  public void setAuthenticator(Authenticator _authenticator) {
-    this.authenticator = _authenticator;
-  }
-
-  /**
-   * (GETobjects extension):
-   * Returns the authenticator currently in use for this connection.
-   * The default is to use the URLAuthorityBasedAuthenticator.
-   *
-   * @see org.getobjects.appserver.core.WOHTTPConnection.URLAuthorityBasedAuthenticator
-   */
-  public Authenticator authenticator() {
-    return this.authenticator;
-  }
-
+ 
   // Setup
 
   protected void setupURL() {
-    String protocol = this.url.getProtocol().toLowerCase();
+    final String protocol = this.url.getProtocol().toLowerCase();
     if (protocol.equals("https"))
       this.isSSL = true;
     else if (!protocol.equals("http"))
       log.warn("given URL not for HTTP connection: " + this.url);
   }
 
-  /**
-   * (GETobjects extension):
-   * Called, whenever a new urlConnection is setup.
-   *
-   * Subclasses may override or extend the steps taken.
-   */
-  protected void setupHttpURLConnection(HttpURLConnection _conn) {
-    // According to WO53 documentation, this is indeed the first sendTimeout
-    _conn.setConnectTimeout(this.sendTimeout);
-    _conn.setAllowUserInteraction(false);
-    _conn.setDoInput(true);
-  }
-
-  /**
-   * (GETobjects extension):
-   * Called, whenever a new urlConnection is setup and only if it's a
-   * HttpsURLConnection.
-   *
-   * This method is called in addition to
-   * setupHttpURLConnection() and should contain refinement necessary for
-   * SSL purposes, only.
-   *
-   * Subclasses may override or extend the steps taken.
-   */
-  protected void setupHttpsURLConnection(HttpsURLConnection _conn) {
-    _conn.setHostnameVerifier(this);
-
-    if (this.allowInsecureSSL) {
-      try {
-        _conn.setSSLSocketFactory(new GullibleSSLSocketFactory(null));
-      }
-      catch (GeneralSecurityException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  // SSL
-
-  public boolean verify(String _hostname, SSLSession _session) {
-    // TODO: tie this to allowInsecureSSL and really validate hostname?
-    return true;
+  protected Authentication getAuthentication(URL _rqURL) {
+    return AllRealmsBasicAuthentication.createForURL(_rqURL);
   }
 
   // Request
@@ -255,58 +184,77 @@ public class WOHTTPConnection extends NSObject implements HostnameVerifier {
     if (_rq == null) return false;
 
     try {
-      URL     rqURL      = new URL(this.url, _rq.uri());
-      boolean needsSetup = true;
-
-      if (this.urlConnection != null &&
-          this.urlConnection.getURL().equals(rqURL))
-      {
-        needsSetup = false;
-      }
+      final URL     rqURL      = new URL(this.url, _rq.uri());
+      final boolean needsSetup = (this.httpClient == null);
 
       if (needsSetup) {
-        this.urlConnection = (HttpURLConnection)rqURL.openConnection();
-
-        this.setupHttpURLConnection(this.urlConnection);
-        if (this.urlConnection instanceof HttpsURLConnection) {
-          this.setupHttpsURLConnection((HttpsURLConnection)this.urlConnection);
+        if (this.isSSL) {
+          final SslContextFactory sslCtxFactory =
+              new SslContextFactory(this.allowInsecureHttpsConnection());
+          this.httpClient = new HttpClient(sslCtxFactory);
         }
+        else {
+          this.httpClient = new HttpClient();
+        }
+
+        final Authentication auth = getAuthentication(rqURL);
+        if (auth != null) {
+          final AuthenticationStore authStore =
+              this.httpClient.getAuthenticationStore();
+          authStore.addAuthentication(auth);
+        }
+        this.httpClient.start();
       }
 
-      // unfortunately there's no better API...
-      Authenticator.setDefault(this.authenticator());
+      // httpClient provides its copy of "user-agent" to all requests it
+      // creates - which also includes requests created while following
+      // redirects
 
+      String userAgent = _rq.headerForKey("user-agent");
+      if (UObject.isNotEmpty(userAgent)) {
+        this.httpClient.setUserAgentField(
+            new HttpField("user-agent", userAgent));
+      }
+
+      final Request rq = this.httpClient.newRequest(rqURL.toString());
+      
       // set properties and headers
-      this.urlConnection.setRequestMethod(_rq.method());
-      this.urlConnection.setInstanceFollowRedirects(this.followRedirects);
+      rq.method(_rq.method());
+      rq.followRedirects(this.followRedirects());
+      
       for (String headerKey : _rq.headerKeys()) {
-        List<String> headers = _rq.headersForKey(headerKey);
+        // filter "user-agent", see above
+        if ("user-agent".equals(headerKey.toLowerCase()))
+          continue;
+        final List<String> headers = _rq.headersForKey(headerKey);
         if (UObject.isNotEmpty(headers)) {
-          String headerProp = UString.componentsJoinedByString(headers, " , ");
-          this.urlConnection.setRequestProperty(headerKey, headerProp);
+          for (String header : headers)
+            rq.header(headerKey, header);
         }
       }
+      final String connectionProp = this.keepAlive ? "keep-alive" : "close";
+      rq.header("connection", connectionProp);
 
-      String connectionProp = this.keepAlive ? "keep-alive" : "close";
-      this.urlConnection.setRequestProperty("connection", connectionProp);
-
-      byte[] content = _rq.content();
-      boolean hasContent = UObject.isNotEmpty(content);
-      // NOTE: setting doOutput(true) has the effect of also setting
-      // the request method to POST? WTF?
-      this.urlConnection.setDoOutput(hasContent);
-      if (hasContent) {
-        // send content body
-        OutputStream os = this.urlConnection.getOutputStream();
-        os.write(content);
-        os.close();
+      for (WOCookie cookie : _rq.cookies()) {
+        final HttpCookie httpCookie =
+            new HttpCookie(cookie.name(), cookie.value());
+        httpCookie.setPath(cookie.path());
+        httpCookie.setDomain(cookie.domain());
+        httpCookie.setMaxAge(cookie.timeOut());
+        httpCookie.setSecure(cookie.isSecure);
       }
-      else {
-        this.urlConnection.connect();
+
+      rq.timeout(sendTimeout() + receiveTimeout() + readTimeout(), TimeUnit.MILLISECONDS);
+ 
+      final byte[] content = _rq.content();
+      final boolean hasContent = (content != null) && (content.length > 0);
+      if (hasContent) {
+        rq.content(new BytesContentProvider(content));
       }
 
       // save request for response
       this.request = _rq;
+      this.contentResponse = rq.send();
       return true;
     }
     catch (Exception e) {
@@ -317,199 +265,76 @@ public class WOHTTPConnection extends NSObject implements HostnameVerifier {
 
   // Response
 
-  protected static void attachToResponse(HttpURLConnection _urlConnection,
-      WOResponse _r)
-  {
-    String httpVersion = null;
-    int    status      = WOMessage.HTTP_STATUS_INTERNAL_ERROR;
-
-    // retrieve status and HTTP version
-    String resp = _urlConnection.getHeaderField(0);
-    if (resp != null) {
-      String[] fields = resp.split(" ");
-
-      if (fields.length >= 2) {
-        try {
-          httpVersion = fields[0];
-          status      = Integer.parseInt(fields[1]);
-        }
-        catch (Exception e) {
-          try {
-            status = _urlConnection.getResponseCode();
-          }
-          catch (IOException e1) {
-          }
-        }
-      }
-    }
-
-    // apply status
-    _r.setStatus(status);
-    // kinda hackish
-    _r.httpVersion = httpVersion;
-
-    for (int i = 1; _urlConnection.getHeaderField(i) != null; i++) {
-      String headerProp = _urlConnection.getHeaderField(i);
-      String headerKey  = _urlConnection.getHeaderFieldKey(i).toLowerCase();
-      String[] headerValues = headerProp.split(" , ");
-      if (headerValues.length == 1) {
-        _r.setHeaderForKey(headerProp, headerKey);
-      }
-      else {
-        List<String> header = new ArrayList<String>(headerValues.length);
-        for (String value : headerValues) {
-          header.add(value.trim());
-        }
-        _r.setHeadersForKey(header, headerKey);
-      }
-    }
-  }
-
   public WOResponse readResponse() {
-    WOResponse r = new WOResponse(this.request);
+    final WOResponse r = new WOResponse(this.request);
+    r.removeHeadersForKey("content-type");
     try {
-      this.urlConnection.setReadTimeout(this.readTimeout);
+      r.setStatus(this.contentResponse.getStatus());
 
-      // read content body
-
-      InputStream is  = this.urlConnection.getInputStream();
-      // TODO: in order to properly support receiveTimeout, we'd need
-      // to implement stream loading here and check the receiveTimeout
-      // exhaustion for all successive read operations.
-      byte[] content = UData.loadContentFromStream(is);
+      final HttpFields headers = this.contentResponse.getHeaders();
+      for (HttpField header : headers) {
+        final List<String> allHeaders = r.headersForKey(header.getName());
+        allHeaders.add(header.getValue());
+        r.setHeadersForKey(allHeaders, header.getName());
+      }
+      final byte[] content = this.contentResponse.getContent();
       r.setContent(content);
 
-      attachToResponse(this.urlConnection, r);
-
-      String connValue = r.headerForKey("connection");
+      final String connValue = r.headerForKey("connection");
       if (UObject.isNotEmpty(connValue)) {
         if (connValue.toLowerCase().equals("close")) {
-          this.urlConnection.disconnect();
-          // NOTE: it's ok to do this here, as this is the last step and
-          // a disconnected urlConnection cannot be reused anyways
-          this.urlConnection = null;
+          this.httpClient.stop();
+          this.httpClient = null;
         }
       }
     }
     catch (Exception e) {
-      if (this.urlConnection != null) {
-        attachToResponse(this.urlConnection, r);
-      }
-      else {
-        r.setStatus(WOMessage.HTTP_STATUS_INTERNAL_ERROR);
-      }
+      r.setStatus(WOMessage.HTTP_STATUS_INTERNAL_ERROR);
       log.error("readResponse() failed: " + e);
     }
+    this.contentResponse = null;
     return r;
   }
-
-
-  // SSL Helpers
-
-  /**
-   * GullibleSSLSocketFactory provides SSL sockets which accept all server
-   * certificates.
-   *
-   * Apart from performing encryption, no 'trust' checking is done on any
-   * certificate involved. Connections using these sockets are vulnerable
-   * to man in the middle attacks!
-   */
-  protected class GullibleSSLSocketFactory extends SSLSocketFactory {
-
-    /**
-     * GullibleTrustManager doesn't implement any trust management at all.
-     */
-    protected class GullibleTrustManager implements X509TrustManager {
-
-      public X509Certificate[] getAcceptedIssuers() {
-          return new X509Certificate[0];
-      }
-
-      public void checkClientTrusted(X509Certificate[] _certs, String _authType)
-      {
-      }
-
-      public void checkServerTrusted(X509Certificate[] _certs, String _authType)
-      {
-      }
+  
+  protected static class AllRealmsBasicAuthentication
+    extends BasicAuthentication
+  {
+    public AllRealmsBasicAuthentication(URI uri, String user, String password){
+      super(uri, null, user, password);
     }
 
-    protected SSLSocketFactory factory;
-
-    public GullibleSSLSocketFactory(String _unused)
-    throws GeneralSecurityException
-    {
-        SSLContext ctx = SSLContext.getInstance("TLS");
-        ctx.init(null, new TrustManager[] { new GullibleTrustManager() }, null);
-        this.factory = ctx.getSocketFactory();
-    }
-
-    public Socket createSocket(InetAddress _host, int _port) throws IOException
-    {
-      return this.factory.createSocket(_host, _port);
-    }
-
-    public Socket createSocket(String _host, int _port) throws IOException {
-      return this.factory.createSocket(_host, _port);
-    }
-
-    public Socket createSocket(String _host, int _port, InetAddress localHost,
-        int localPort) throws IOException
-    {
-      return this.factory.createSocket(_host, _port, localHost, localPort);
-    }
-
-    public Socket createSocket(InetAddress _address, int _port,
-        InetAddress _localAddress, int _localPort) throws IOException
-    {
-      return this.factory.createSocket(_address, _port, _localAddress,
-          _localPort);
-    }
-
-    public Socket createSocket(Socket _socket, String _host, int _port,
-        boolean _autoClose) throws IOException
-    {
-      return this.factory.createSocket(_socket, _host, _port, _autoClose);
-    }
-
-    public String[] getDefaultCipherSuites() {
-      return this.factory.getDefaultCipherSuites();
-    }
-
-    public String[] getSupportedCipherSuites() {
-      return this.factory.getSupportedCipherSuites();
-    }
-  }
-
-  // Authenticator
-
-  /**
-   * This authenticator uses the "authority" part of a URL to provide
-   * the required authentication, i.e. for a URL like this:
-   * <code>
-   * URL url = new URL("http://foo:bar@example.org");
-   * </code>
-   * this authenticator would authenticate as user "foo" with password "bar".
-   */
-  protected static class URLAuthorityBasedAuthenticator extends Authenticator {
-    @Override
-    protected PasswordAuthentication getPasswordAuthentication() {
-      String auth = getRequestingURL().getAuthority();
-      if (auth != null) {
-        int idx = auth.indexOf("@");
+    public static AllRealmsBasicAuthentication createForURL(URL url) {
+      String auth = url.getAuthority();
+      if (auth == null)
+        return null;
+    
+      String user = "";
+      String password = "";
+      int idx = auth.indexOf("@");
+      if (idx != -1) {
+        auth = auth.substring(0, idx);
+        idx  = auth.indexOf(":");
         if (idx != -1) {
-          auth = auth.substring(0, idx);
-          idx  = auth.indexOf(":");
-          if (idx != -1) {
-            String user     = auth.substring(0, idx);
-            String password = auth.substring(idx + 1);
-            return new PasswordAuthentication(user, password.toCharArray());
-          }
-          return new PasswordAuthentication(auth, "".toCharArray());
+          user     = auth.substring(0, idx);
+          password = auth.substring(idx + 1);
         }
-        return new PasswordAuthentication("", "".toCharArray());
+        else {
+          user = auth;
+        }
       }
-      return super.getPasswordAuthentication();
+      URI uri = null;
+      try {
+        uri = url.toURI();
+      }
+      catch (URISyntaxException e) {} // never happens
+      return new AllRealmsBasicAuthentication(uri, user, password);
+    }
+    
+    @Override
+    public boolean matches(String type, URI uri, String realm) {
+      if (!"basic".equalsIgnoreCase(type))
+        return false;
+      return true;
     }
   }
 }
