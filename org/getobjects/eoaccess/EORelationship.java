d26696be241ac67496baaade4512e8a2bbe5a84c
Marcus Müller <znek@mulle-kybernetik.com>
2019-05-09 19:38:38 +0200

    finally & cleanup

--- org/getobjects/eoaccess/EORelationship.java
+++ org/getobjects/eoaccess/EORelationship.java
@@ -39,12 +39,12 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
   protected int      joinSemantic;
   protected boolean  isToMany;
   protected String   relationshipPath;
-  
+
   /* construction */
-  
+
   public EORelationship
-    (String _name, boolean _isToMany, EOEntity _src, String _dest,
-     EOJoin[] _joins)
+    (final String _name, final boolean _isToMany,
+     final EOEntity _src, final String _dest, final EOJoin[] _joins)
   {
     this.name                  = _name;
     this.entity                = _src;
@@ -52,33 +52,33 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
     this.joins                 = _joins;
     this.isToMany              = _isToMany;
   }
-  
-  
+
   /* accessors */
-  
+
   /**
    * Returns the name of the relationship, eg 'employments' or 'owner'.
-   * 
+   *
    * @return name of the relationship
    */
   @Override
   public String name() {
     return this.name;
   }
+
   public boolean isToMany() {
     return this.isToMany;
   }
- 
+
   /**
    * Returns the EOEntity the relationship is a part of. Eg the 'Persons'
    * entity could be the owner of the 'addresses' relationship.
-   * 
+   *
    * @return the EOEntity
    */
   public EOEntity entity() {
     return this.entity;
   }
-  
+
   /**
    * Returns the EOEntity the relationship targets. Eg if the base entity is
    * 'Persons' and the relationship is 'addresses', the destination entity
@@ -86,7 +86,7 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
    * <p>
    * This method requires that the model tree has been resolved (ie that the
    * connectRelationshipsInModel() has been called).
-   * 
+   *
    * @return the target entity
    */
   public EOEntity destinationEntity() {
@@ -98,13 +98,13 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
    * to the destination entity (eg 'Addresses'). Usually the array will contain
    * just one EOJoin, one which contains the primary key of the base entity
    * and the matching foreign key in the destination entity.
-   * 
+   *
    * @return an array of EOJoin's which need to be performed to connect the two
    */
   public EOJoin[] joins() {
     return this.joins;
   }
-  
+
   /**
    * Returns the join semantics to be used. Its one of:
    * <ul>
@@ -115,17 +115,17 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
    * </ul>
    * The default is InnerJoin, but most likely LeftOuterJoin is the one which
    * is most appropriate for most optional toOne and most toMany relationships.
-   * 
+   *
    * @return the constant for the join semantics
    */
   public int joinSemantic() {
     return this.joinSemantic;
   }
-  
+
   /**
    * Returns true if the EORelationship requires more than one EOJoin
    * expression.
-   * 
+   *
    * @return true if there is more than one EOJoin
    */
   public boolean isCompound() {
@@ -135,136 +135,150 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
   /**
    * Returns true if this is a flattened relationship. A flattened relationship
    * is a shortcut for a longer path. For example to retrieve the addresses of
-   * the companies assigned to a person you could use:<pre>
-   *   employments.company.addresses</pre>
+   * the companies assigned to a person you could use:
+   *
+   * <pre>
+   * employments.company.addresses
+   * </pre>
    * <p>
    * Or you could create a 'flattened' relationship 'companyAddresses' which
    * contains this path. The path of the real relationship can be retrieved
    * using the relationshipPath() method.
-   * 
+   *
    * @return true if this is a flattened relationship
    */
   public boolean isFlattened() {
     return this.relationshipPath != null;
   }
-  
+
   /**
    * Returns the relationship path of a flattened relationship. Eg if the
    * flattened relationship is 'companyAddresses', the path might be
    * 'employments.company.addresses'.
    * If there is no relationship path, the EORelationship returns its name.
-   * 
+   *
    * <p>
    * Note: this is called by 'levelPrefetchSpecification' in EODatabaseChannel.
-   * 
+   *
    * @return the real relationship path, eg (employments.company.addresses)
    */
   @Override
   public String relationshipPath() {
     return this.relationshipPath != null ? this.relationshipPath : this.name();
   }
-  
+
   /**
    * Returns the EORelationship objects for each component of the
-   * relationshipPath() of a flattened EORelationship. Eg: <pre>
-   *   employments.company.addresses</pre>
-   * could return three EORelationship objects:<pre>
+   * relationshipPath() of a flattened EORelationship. Eg:
+   *
+   * <pre>
+   * employments.company.addresses
+   * </pre>
+   *
+   * could return three EORelationship objects:
+   *
+   * <pre>
    *   'employments', source = Persons,     dest = Employments
    *   'company',     source = Employments, dest = Companies
-   *   'addresses',   source = Companies,   dest = Addresses</pre>
-   * 
+   *   'addresses',   source = Companies,   dest = Addresses
+   * </pre>
+   *
    * The method returns null if this is not a flattened relationship.
-   * 
+   *
    * @return the array of relationships forming the flattened path
    */
   public EORelationship[] componentRelationships() {
     if (this.relationshipPath == null)
       return null;
-    
+
     EOEntity relentity = this.entity();
     if (relentity == null)
       return null;
-    
-    String[] path =
+
+    final String[] path =
       NSKeyValueCodingAdditions.Utility.splitKeyPath(this.relationshipPath);
-    
-    EORelationship[] relships = new EORelationship[path.length]; 
+
+    final EORelationship[] relships = new EORelationship[path.length];
     for (int i = 0; i < path.length; i++) {
       if (relentity == null)
         return null; // TBD: log
-      
+
       relships[i] = relentity.relationshipNamed(path[i]);
       if (relships[i] == null) // TBD: log
         return null;
-      
+
       if (relships[i].isFlattened())
         ; // TBD: pathes containing flattened relships
-      
+
       relentity = relships[i].destinationEntity();
     }
-    
+
     return relships;
   }
-  
+
   /**
    * Makes the EORelationship check whether any of its joins reference the
    * given property.
    * A property is an EOAttribute or EORelationship object.
-   * 
+   *
+   *
    * @param _property - property to check for
    * @return true if any join refers the property
    */
-  public boolean referencesProperty(Object _property) {
+  public boolean referencesProperty(final Object _property) {
     if (_property == null)
       return false;
-    
+
     if (this.joins != null) {
       for (int i = 0; i < this.joins.length; i++) {
         if (this.joins[i].referencesProperty(_property))
           return true;
       }
     }
-    
+
     if (this.relationshipPath != null) {
-      EORelationship[] props = this.componentRelationships();
+      final EORelationship[] props = this.componentRelationships();
       if (props != null) {
-        for (EORelationship r: props) {
+        for (final EORelationship r : props) {
           if (r == _property)
             return true;
           // TBD: do we need to call referencesProperty on the relationships?
         }
       }
     }
-    
+
     return false;
   }
 
   /**
    * Checks whether the EORelationship got resolved (whether the EOEntity of
    * the destination entity was looked up).
-   * 
+   *
    * @return true of the destinationEntity is assigned, false otherwise
    */
   public boolean isConnected() {
     return this.destinationEntity != null;
   }
-  public void connectRelationshipsInModel(EOModel _model, EOEntity _entity) {
+
+  public void connectRelationshipsInModel
+    (final EOModel _model, final EOEntity _entity)
+  {
     this.entity = _entity;
-    
+
     if (this.destinationEntityName == null)
       return;
-    
+
     this.destinationEntity = _model.entityNamed(this.destinationEntityName);
-    
+
     if (this.joins != null) {
       for (int i = 0; i < this.joins.length; i++)
         this.joins[i].connectToEntities(this.entity, this.destinationEntity);
     }
   }
-  
+
   /**
    * Locates an inverse relationship in the destination entity.
-   * 
+   *
    * Example: n:1
    *   person  ( person_id, company_id )
    *   company ( company_id )
@@ -272,14 +286,14 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
    *   toCompany [toOne] ( SRC.company_id = TAR.company_id )
    * Company:
    *   toPerson [toMany] ( SRC.company_id = TAR.company_id )
-   * 
+   *
    * @return the inverse relationship
    */
   public EORelationship inverseRelationship() {
     // TBD: implement me
     // TBD: consider N:M relationships
     // find a relationship in the target which joins the same columns
-    EOJoin[] myJoins = this.joins();
+    final EOJoin[] myJoins = this.joins();
     if (myJoins == null || myJoins.length == 0)
       return null; /* we have no joins?! */
     if (myJoins.length > 1) {
@@ -288,105 +302,106 @@ public class EORelationship extends EOProperty implements EOExpressionEvaluation
         ("ERROR: not supporting inverse relationships with multiple joins yet");
       return null;
     }
-    
-    EOEntity myEntity = this.entity();
+
+    final EOEntity myEntity = this.entity();
     if (myEntity == null)
       return null; /* we have no entity? */
-    
-    EOEntity dest = this.destinationEntity();
-    if (dest == null) return null; // TBD: log?
-    
-    EORelationship[] rels = dest.relationships();
-    if (rels == null || rels.length == 0) return null; /* none found */
-    
-    for (EORelationship rel: rels) {
+
+    final EOEntity dest = this.destinationEntity();
+    if (dest == null)
+      return null; // TBD: log?
+
+    final EORelationship[] rels = dest.relationships();
+    if (rels == null || rels.length == 0)
+      return null; /* none found */
+
+    for (final EORelationship rel : rels) {
       if (myEntity != rel.destinationEntity())
         continue; /* other entity, does not point back */
-      
-      EOJoin[] relJoins = rel.joins();
+
+      final EOJoin[] relJoins = rel.joins();
       if (relJoins == null || relJoins.length != this.joins.length)
         continue; /* join array sizes do not match */
-      
+
       // TBD: we only support one join for now ...
-      EOJoin myJoin = myJoins[0];
-      EOJoin enemy  = relJoins[0];
-      
+      final EOJoin myJoin = myJoins[0];
+      final EOJoin enemy  = relJoins[0];
+
       // TBD: equality might not be correct since joins can be directed
       //      (relevant for LEFT/RIGHT joins I guess)
       if (myJoin == enemy || myJoin.equals(enemy) ||
           myJoin.isReciprocalToJoin(enemy))
         return rel;
     }
-    
+
     return null;
   }
-  
-  
+
   /* patterns */
-  
+
   /**
    * Returns true if this is a pattern relationship.
    * <p>
    * We do not support pattern relationships yet.
-   * 
+   *
    * @return false
    */
   public boolean isPatternRelationship() {
     return false;
   }
-  
-  
+
   /* names */
-  
+
   public void beautifyNames() {
     // TODO
     // probably this belongs into some external objects which implements a
     // specific 'beautification policy', eg RoR or EOF.
   }
-  
-  
+
   /* constants */
-  
+
   static final int FullOuterJoin  = 1;
   static final int InnerJoin      = 2;
   static final int LeftOuterJoin  = 3;
   static final int RightOuterJoin = 4;
-  
+
   /* EOValueEvaluation */
-  
+
+  @Override
   public Object valueForObject(final Object _o) {
     return NSKeyValueCodingAdditions.Utility.valueForKeyPath(_o, this.name());
   }
-  
+
   /* description */
-  
+
   @Override
   public void appendAttributesToDescription(final StringBuilder _d) {
     super.appendAttributesToDescription(_d);
 
     if (this.isPatternRelationship())
       _d.append(" pattern");
-    
-    if (this.name != null) _d.append(" name=" + this.name);
-    
-    if (this.entity != null) 
+
+    if (this.name != null)
+      _d.append(" name=" + this.name);
+
+    if (this.entity != null)
       _d.append(" from=" + this.entity.name());
     if (this.destinationEntity != null)
       _d.append(" to=" + this.destinationEntity.name());
-    
+
     if (this.joins != null && this.joins.length > 0) {
       _d.append(" join=");
-      for (EOJoin join: this.joins) {
+      for (final EOJoin join : this.joins) {
         _d.append('[');
-        
+
         EOAttribute a = join.sourceAttribute();
         _d.append(a != null ? a.name() : "null");
-        
+
         _d.append("=>");
-        
+
         a = join.destinationAttribute();
         _d.append(a != null ? a.name() : "null");
-        
+
         _d.append(']');
       }
     }
