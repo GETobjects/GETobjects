9392e5145a96793b89de4050310ec536aac7a4bf
Marcus Müller <znek@mulle-kybernetik.com>
2020-05-07 15:52:31 +0200

    Cleanup formatting and add final

--- org/getobjects/appserver/core/WOCookie.java
+++ org/getobjects/appserver/core/WOCookie.java
@@ -38,9 +38,9 @@ import org.getobjects.foundation.NSObject;
  * To check for cookies coming in, use cookieValueForKey() or
  * cookieValuesForKey() of WORequest, eg:<pre>
  *   System.err.println("Cookie: " + _rq.cookieValueForKey("myCookie"));</pre>
- * 
+ *
  * Remember that browsers limit the size and the number of cookies per site.
- * 
+ *
  * <p>
  * To pass a cookie between hosts you can use the domain, eg:<pre>
  *   domain=.zideone.com</pre>
@@ -50,7 +50,7 @@ import org.getobjects.foundation.NSObject;
  *   etc</pre>
  * Note: if the path is not set, the request path will be used! This is usually
  *       not what you want.
- * 
+ *
  * <p>
  * http://www.ietf.org/rfc/rfc2109.txt<br>
  * http://www.faqs.org/rfcs/rfc2965.html (only implemented by Opera?)
@@ -66,8 +66,8 @@ public class WOCookie extends NSObject {
   protected int     timeout;  /* in seconds (-1 == do not expire) */
   protected boolean isSecure; /* whether cookie requires an HTTPS connection */
 
-  public WOCookie(String _name, String _value, String _path, String _domain, 
-                  Date _date, boolean _isSecure)
+  public WOCookie(final String _name, final String _value, final String _path, final String _domain,
+                  final Date _date, final boolean _isSecure)
   {
     this.name     = _name;
     this.value    = _value;
@@ -78,8 +78,8 @@ public class WOCookie extends NSObject {
     this.timeout  = -1; /* do not expire cookie (use date) */
   }
 
-  public WOCookie(String _name, String _value, String _path, String _domain, 
-                  int _timeoutInS, boolean _isSecure)
+  public WOCookie(final String _name, final String _value, final String _path, final String _domain,
+                  final int _timeoutInS, final boolean _isSecure)
   {
     this.name     = _name;
     this.value    = _value;
@@ -89,8 +89,8 @@ public class WOCookie extends NSObject {
     this.isSecure = _isSecure;
   }
 
-  public WOCookie(String _name, String _value, String _path, String _domain, 
-                  boolean _isSecure)
+  public WOCookie(final String _name, final String _value, final String _path, final String _domain,
+                  final boolean _isSecure)
   {
     this.name     = _name;
     this.value    = _value;
@@ -99,25 +99,25 @@ public class WOCookie extends NSObject {
     this.isSecure = _isSecure;
     this.timeout  = -1; /* do not expire cookie */
   }
-  
-  public WOCookie(String _name, String _value) {
+
+  public WOCookie(final String _name, final String _value) {
     this.name     = _name;
     this.value    = _value;
     this.timeout  = -1; /* do not expire cookie */
     this.isSecure = false;
   }
-  
-  
+
+
   /* accessors */
-  
-  public void setName(String _s) {
+
+  public void setName(final String _s) {
     this.name = _s;
   }
   public String name() {
     return this.name;
   }
-  
-  public void setValue(String _s) {
+
+  public void setValue(final String _s) {
     if (_s != null && _s.length() > 0 && _s.charAt(0) == '$')
       log.warn("cookie value may not start with a dollar sign!: " + _s);
     this.value = _s;
@@ -125,7 +125,7 @@ public class WOCookie extends NSObject {
   public String value() {
     return this.value;
   }
-  
+
   /**
    * Sets the cookie-timeout aka the 'Max-Age' attribute of RFC 2109.
    * <ol>
@@ -133,30 +133,30 @@ public class WOCookie extends NSObject {
    *   <li>if the value is 0, this tells the browser to expire the cookie
    *   <li>if the value is &gt; 0, the browser will remove the cookie
    * </ol>
-   *  
+   *
    * @param _date
    */
-  public void setTimeOut(int _date) {
+  public void setTimeOut(final int _date) {
     this.timeout = _date;
   }
   public int timeOut() {
     return this.timeout;
   }
-  
+
   /**
    * Sets the 'expires' date for the cookie, see RFC 2109. This is deprecated,
    * use setTimeOut (Max-Age) instead.
-   * 
+   *
    * @param _date - the Date when the cookie expires
    */
-  public void setExpires(Date _date) {
+  public void setExpires(final Date _date) {
     this.date = _date;
   }
   public Date expires() {
     return this.date;
   }
-  
-  public void setDomain(String _s) {
+
+  public void setDomain(final String _s) {
     if (_s != null && !_s.startsWith("."))
       log.warn("cookie domain does not start with a dot!: " + _s);
     this.domain = _s;
@@ -164,8 +164,8 @@ public class WOCookie extends NSObject {
   public String domain() {
     return this.domain;
   }
-  
-  public void setPath(String _s) {
+
+  public void setPath(final String _s) {
     this.path = _s;
   }
   public String path() {
@@ -178,20 +178,20 @@ public class WOCookie extends NSObject {
   /**
    * Returns the HTTP response representation of the cookie value, w/o the
    * header name ("cookie").
-   * 
+   *
    * @return the HTTP String representing the WOCookie
    */
   public String headerString() {
-    StringBuilder sb = new StringBuilder(256);
-    
+    final StringBuilder sb = new StringBuilder(256);
+
     // TODO: do we need to escape the value?
-    sb.append(this.name());
+    sb.append(name());
     sb.append("=");
-    
-    String v = this.value();
+
+    final String v = value();
     if (v != null) sb.append(v);
     sb.append("; version=\"1\""); // v1 means RFC 2109
-    
+
     if (this.path != null) {
       sb.append("; path=");
       sb.append(this.path);
@@ -200,13 +200,13 @@ public class WOCookie extends NSObject {
       sb.append("; domain=");
       sb.append(this.domain);
     }
-    
-    int to = this.timeOut();
+
+    final int to = timeOut();
     if (to >= 0) {
       sb.append("; Max-Age=");
       sb.append(to);
     }
-    if (this.expires() != null) {
+    if (expires() != null) {
       // TBD: Wdy, DD-Mon-YY HH:MM:SS GMT
       log.warn("WOCookie does not yet support expires, and you should use " +
           "setTimeOut() anyways (aka Max-Age)");
@@ -219,17 +219,17 @@ public class WOCookie extends NSObject {
        */
       sb.append("; expires=Wednesday, 09-Nov-99 23:12:40 GMT");
     }
-    
+
     if (this.isSecure)
       sb.append("; secure");
     
     return sb.toString();
   }
-  
-  
+
+
   /* parsing cookies */
-  
-  public static WOCookie parseCookieString(String _s) {
+
+  public static WOCookie parseCookieString(final String _s) {
     if (_s == null) return null;
 
     int idx = _s.indexOf('=');
@@ -237,26 +237,27 @@ public class WOCookie extends NSObject {
       log.warn("got invalid cookie value: '" + _s + "'");
       return null;
     }
-    
+
     String name  = _s.substring(0, idx);
     String value = _s.substring(idx + 1);
-    
+
     // TODO: process escaping
-    
+
     idx = _s.indexOf(';');
     if (idx == -1)
       return new WOCookie(name, value);
 
     /* process options */
-    
-    String[] opts = value.substring(idx + 1).split(";");
+
+    final String[] opts = value.substring(idx + 1).split(";");
     value = value.substring(0, idx);
-    
-    String  path = null, domain = null;
+
+    String  path = null;
+    final String domain = null;
     boolean isSecure = false;
     
     for (int i = 0; i < opts.length; i++) {
-      String opt = opts[i].trim();
+      final String opt = opts[i].trim();
       if (opt.startsWith("domain="))
         name = opt.substring(7);
       else if (opt.startsWith("path="))
@@ -270,9 +271,9 @@ public class WOCookie extends NSObject {
     return new WOCookie(name, value, path, domain, isSecure);
   }
 
-  
+
   /* description */
-  
+
   @Override
   public void appendAttributesToDescription(final StringBuilder _d) {
     if (this.name != null) {
@@ -283,7 +284,7 @@ public class WOCookie extends NSObject {
       _d.append(" value=");
       _d.append(this.value);
     }
-    
+
     if (this.path != null) {
       _d.append(" path=");
       _d.append(this.path);
@@ -292,7 +293,7 @@ public class WOCookie extends NSObject {
       _d.append(" domain=");
       _d.append(this.domain);
     }
-    
+
     if (this.timeout == 0)
       _d.append(" delete-cookie");
     else if (this.timeout > 0) {
@@ -304,26 +305,26 @@ public class WOCookie extends NSObject {
     if (this.isSecure)
       _d.append(" secure");
   }
-  
-  
+
+
   /* utility */
-  
+
   public static void addCookieInfo
     (final Collection<WOCookie> _cookies, final StringBuilder _sb)
   {
     boolean isFirst = true;
-    
-    for (WOCookie cookie: _cookies) {
+
+    for (final WOCookie cookie: _cookies) {
       if (isFirst) isFirst = false;
       else _sb.append(",");
-      
+
       if (cookie.timeOut() == 0) { /* expire */
         _sb.append("-");
         _sb.append(cookie.name());
       }
       else {
         _sb.append(cookie.name());
-        String v = cookie.value();
+        final String v = cookie.value();
         if (v != null) {
           _sb.append("=");
           if (v.length() < 12)
